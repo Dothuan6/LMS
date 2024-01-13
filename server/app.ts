872aabcf58f477c2ca3a3c2f28693a57316136c2
Dothuan6 <thuann6222@gmail.com>
2024-01-13 22:54:32 +0700

    router for accessing courese

--- server/app.ts
+++ server/app.ts
@@ -1,37 +1,40 @@
-require('dotenv').config();
-import express,{NextFunction,Request,Response} from "express";
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import {ErrorMiddleware} from "./middleware/error";
-import userRouter from './routes/user.route';
-
+import { ErrorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
+import courseRouter from "./routes/course.route";
 
 //body parser
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
 
 //cookie parser
 app.use(cookieParser());
 
 //cors => cross origin resource sharing
-app.use(cors({
-    origin: process.env.ORIGIN
-}));
-
-app.use("/api/v1",userRouter);
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+//routes
+app.use("/api/v1", userRouter);
+app.use("/api/v1", courseRouter);
 
 //testing api
-app.get("/test",(req,res,next)=> {
-    res.status(200).json({
-        success: true,
-        message : "API is working",
-    });
+app.get("/test", (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
 });
 
 //unknow
-app.all("*",(req,res,next)=>{
-    const err = new Error(`Route ${req.originalUrl} not found`) as any;
-    err.statusCode  = 404;
-    next(err);
-})
+app.all("*", (req, res, next) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
 app.use(ErrorMiddleware);
